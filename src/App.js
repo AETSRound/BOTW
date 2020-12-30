a7edd8fe054f767c31d51ef3cfa956d35df7bf72
AETSRound <qkrcksdna@naver.com>
2020-12-30 11:00:52 +0900

    아이템창 크키 키우고, 이미지 추가 및 아이템 수량 정보 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import IG from './ItemGrid'
 function App() {
   return (
     <div className="App">
-     <h2>hello</h2>
+      <h1>젤다 UI 만들기</h1>
+      <h3>아이템 창</h3>
       <IG></IG>
     </div>
   );
--- src/Item.js
+++ src/Item.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import weapon1 from './items/weapons/BotW_Ancient_Short_Sword_Icon.png';
 
 export default class Item extends Component {
    
     render() {
+        const itemQty =  this.props.ItemName
         return (
             <span>
-                <InnerItem>{this.props.ItemName}</InnerItem>
+                <InnerItem>
+                    <ImgHolder><img src={weapon1}></img></ImgHolder>
+                    <TxtHolder>{this.props.ItemName}</TxtHolder>
+                    {/* {this.props.ItemName} */}
+                </InnerItem>
             </span>
         )
     }
 }
 
 const InnerItem = styled.div`
-    width: 5rem;
-    height: 5rem;
+    width: 6 rem;
+    height: 6rem;
     background: white;
     border:1px solid;
     padding: 5px;
     display:inline-block;
-    border-radius:10%;
+    border-radius:0%;
     font-size:2rem;
+    margin-top: 1rem;
+    margin-left: 1rem;
 `;
+
+const ImgHolder = styled.div`
+
+`;
+
+const TxtHolder = styled.div`
+    position: relative;
+    left:4.4rem;
+    top:-1.4rem;
+    width:2rem;
+    text-align:center;
+    font-size: 1rem;
+    border:1px solid;
+    word-wrap:normal;
+    background-color:brown;
+    color:white;
+`;
+
